1d40e98d95d3f82e27e8eceef10be12f7808ac9b
Juan Pablo Veliz Gonzalez <ju.veliz@duocuc.cl>
2025-05-26 02:17:25 -0400

    Update ProfesorController.java

--- edutech.cl/edutech.cl/src/main/java/com/edutech/cl/edutech/cl/controller/ProfesorController.java
+++ edutech.cl/edutech.cl/src/main/java/com/edutech/cl/edutech/cl/controller/ProfesorController.java
@@ -1,48 +1,47 @@
 package com.edutech.cl.edutech.cl.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.edutech.cl.edutech.cl.model.Profesor;
 import com.edutech.cl.edutech.cl.service.ProfesorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/Profesores")
+@RequestMapping("/api/v1/profesores")
 public class ProfesorController {
-    
-    @Autowired
-    private ProfesorService profesorService;
-    
-    @GetMapping
-    public ResponseEntity<List<Profesor>> listar(){
 
+    private final ProfesorService profesorService;
+
+    @Autowired
+    public ProfesorController(ProfesorService profesorService) {
+        this.profesorService = profesorService;
+    }
+
+    // Obtener todos los profesores
+    @GetMapping
+    public ResponseEntity<List<Profesor>> obtenerTodos() {
         List<Profesor> profesores = profesorService.findAll();
-        
+
         if (profesores.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(profesores);
-        
     }
 
+    // Agregar un nuevo profesor
     @PostMapping
-    public Profesor agregarProfesor(@RequestBody Profesor profesor){
-        return profesorService.save(profesor);
+    public ResponseEntity<Profesor> crearProfesor(@RequestBody Profesor profesor) {
+        Profesor nuevoProfesor = profesorService.save(profesor);
+        return ResponseEntity.ok(nuevoProfesor);
     }
 
-    @DeleteMapping("{id}")
+    // Eliminar profesor por ID
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarProfesor(@PathVariable Integer id) {
-        profesorService.delete(id); 
-        return ResponseEntity.noContent().build(); 
-    
+        profesorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
-}
+
