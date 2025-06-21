c68a49920154a066494b55efb7378192f421b68b
Juan Pablo Veliz Gonzalez <ju.veliz@duocuc.cl>
2025-06-21 00:58:41 -0400

    implementacion de HATEOAS y OpenAPI para Asignaturas
    .- Integra HATEOAS y OpenAPI en API asignaturas.
    .- Añade enlaces a respuestas de asignatura.
    .- Documentacion de endpoints con Swagger/OpenAPI.
    .- Modificacion modelo Asignatura para HATEOAS.
    .- Actualizacion servicio con búsqueda por ID.
    .- Corregimiento de Lombok equals/hashCode warning.
    .- Añade dependencias HATEOAS y Springdoc.
    .- Mejora la API para consumidores.

---
+++ Gestion De Asignatura/Asignatura/Asignatura/src/main/java/Asignatura/Asignatura/repository/AsignaturaRepository.java
@@ -0,0 +1,8 @@
+package Asignatura.Asignatura.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Asignatura.Asignatura.model.Asignatura;
+
+public interface AsignaturaRepository extends JpaRepository<Asignatura, Integer> {
+}
