0561c6e9380d861d1639c17096199762eaecbbc7
Juan Pablo Veliz Gonzalez <ju.veliz@duocuc.cl>
2025-06-24 00:56:23 -0400

    Delete Gestion de Matricula/Gestion de Matricula directory
    error de git, no cambio las carpetas solo hizo una encima de otra

--- Gestion de Matricula/Gestion de Matricula/src/main/java/com/edutech/cl/edutech/cl/repository/MatriculaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edutech.cl.edutech.cl.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edutech.cl.edutech.cl.model.Matricula;
-
-public interface MatriculaRepository extends JpaRepository<Matricula,Long> {
-
-}
