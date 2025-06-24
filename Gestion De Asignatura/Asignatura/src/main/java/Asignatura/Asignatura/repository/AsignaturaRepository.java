de16c35669bd6b6cf1afa63c172249f679f1b0f4
Juan Pablo Veliz Gonzalez <ju.veliz@duocuc.cl>
2025-06-24 02:42:49 -0400

    Delete Gestion De Asignatura/Asignatura directory
    .- debido a errores de compilación.
    .- Git no sobre escribió  la carpeta anterior asi que esta se elimina ya que es redundante

--- Gestion De Asignatura/Asignatura/src/main/java/Asignatura/Asignatura/repository/AsignaturaRepository.java
+++
@@ -1,8 +0,0 @@
-package Asignatura.Asignatura.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Asignatura.Asignatura.model.Asignatura;
-
-public interface AsignaturaRepository extends JpaRepository<Asignatura, Integer> {
-}
