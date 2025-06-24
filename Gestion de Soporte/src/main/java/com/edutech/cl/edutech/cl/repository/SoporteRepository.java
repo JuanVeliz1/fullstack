8d5489b1ea1f60139a3921c1bdaa4f7b38388629
zzapata259 <165106719+zzapata259@users.noreply.github.com>
2025-06-24 06:56:09 -0400

    Delete Gestion de Soporte directory

--- Gestion de Soporte/src/main/java/com/edutech/cl/edutech/cl/repository/SoporteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edutech.cl.edutech.cl.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edutech.cl.edutech.cl.model.Soporte;
-
-public interface SoporteRepository extends JpaRepository<Soporte,Long> {
-
-}
